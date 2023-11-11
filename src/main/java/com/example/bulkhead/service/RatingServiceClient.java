fb7c053e93cc4c20f1e3dba4d038d0cf3e557347
AngelaCastillo <angela.castillo@linktic.com>
2023-11-11 15:21:23 -0500

    ✨ feat(bulkhead): add jmeter

--- src/main/java/com/example/bulkhead/service/RatingServiceClient.java
+++ src/main/java/com/example/bulkhead/service/RatingServiceClient.java
@@ -3,6 +3,7 @@ package com.example.bulkhead.service;
 import com.example.bulkhead.dto.ProductRatingDto;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,12 +17,15 @@ public class RatingServiceClient {
     private String ratingService;
 
     @Bulkhead(name = "ratingService", fallbackMethod = "getDefault")
-    public ProductRatingDto getProductRatingDto(int productId){
-        return this.restTemplate.getForEntity(this.ratingService + productId, ProductRatingDto.class)
-                .getBody();
+    public ProductRatingDto getProductRatingDto(int productId) {
+        System.out.println("Invocando servicio de Rating ---> " + productId);
+        return this.restTemplate.getForEntity(this.ratingService + productId,
+                ProductRatingDto.class).getBody();
     }
 
-    public ProductRatingDto getDefault(int productId, Throwable throwable){
+    @Cacheable(cacheNames = "ratings", key = "#productId")
+    public ProductRatingDto getDefault(int productId, Throwable throwable) {
+        System.out.println("Entrando al fallbackMethod ---> " + productId);
         return new ProductRatingDto(0, Collections.emptyList());
     }
 }
