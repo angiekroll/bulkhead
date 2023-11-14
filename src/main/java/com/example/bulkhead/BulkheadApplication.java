21c86bbf20b14947cc24c3e80cfaef484841b97b
AngelaCastillo <angela.castillo@linktic.com>
2023-11-14 11:44:36 -0500

    ✨ feat(bulkhead): enable cache

--- src/main/java/com/example/bulkhead/BulkheadApplication.java
+++ src/main/java/com/example/bulkhead/BulkheadApplication.java
@@ -2,8 +2,10 @@ package com.example.bulkhead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BulkheadApplication {
 
     public static void main(String[] args) {
